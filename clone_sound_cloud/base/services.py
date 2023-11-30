178f18b343ad93090e4a5f9b4f142af0e0d31689
Maxim Manzik <kepkpkep@gmail.com>
2023-11-30 23:21:35 +0200

    create oauth models and admin

---
+++ clone_sound_cloud/base/services.py
@@ -0,0 +1,14 @@
+from django.core.exceptions import ValidationError
+
+
+def get_path_upload_avatar(instance, file):
+    """Path to file avatar"""
+    return f"avatar/{instance.id}/{file}"
+
+
+def validate_size_image(file_obj):
+    megabyte_limit = 2
+    if file_obj.size > megabyte_limit * 1024 * 1024:
+        raise ValidationError(f"Max size for file {megabyte_limit}MB")
+
+
--- clone_sound_cloud/oauth/admin.py
+++ clone_sound_cloud/oauth/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import AuthUser, Follower, SocialLink
+
+
+@admin.register(AuthUser)
+class AuthUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'display_name', 'join_date',)
+    list_display_links = ('email',)
+
+
+@admin.register(SocialLink)
+class SocialLinkAdmin(admin.ModelAdmin):
+    list_display = ('user', 'link',)
+
