d781c072b43ae38ffc2a4ba7bd558d6b1e6f27a5
Maxim Manzik <kepkpkep@gmail.com>
2023-12-02 01:10:26 +0200

    create models and admin for audio_library app.

--- clone_sound_cloud/audio_library/models.py
+++ clone_sound_cloud/audio_library/models.py
@@ -1,10 +1,89 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
+from base.services import (
+    get_path_upload_album, validate_size_image,
+    validate_size_audio, get_path_upload_track, get_path_upload_playlist
+)
+from oauth.models import UserProfile
 
 
+class License(models.Model):
+    """Model licence audio user"""
+    user = models.ForeignKey(
+        UserProfile, on_delete=models.CASCADE, related_name='licenses')
+    text = models.TextField(max_length=1000)
 
 
+class Genre(models.Model):
+    """Model of genre audio"""
+    name = models.CharField(max_length=25, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
+class Album(models.Model):
+    """Model album for audio"""
+    user = models.ForeignKey(
+        UserProfile, on_delete=models.CASCADE, related_name='albums')
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=1000)
+    private = models.BooleanField(default=False)
+    cover = models.ImageField(
+        upload_to=get_path_upload_album, blank=True, null=True,
+        validators=[FileExtensionValidator(allowed_extensions=['jpg']),
+                    validate_size_image]
+    )
 
 
+class Track(models.Model):
+    """Model track"""
+    user = models.ForeignKey(
+        UserProfile, on_delete=models.CASCADE, related_name='tracks')
+    title = models.CharField(max_length=100)
+    license = models.ForeignKey(
+        License, on_delete=models.PROTECT, related_name='license_tracks')
+    genre = models.ManyToManyField(Genre, related_name='track_genres')
+    album = models.ForeignKey(
+        Album, on_delete=models.SET_NULL, blank=True, null=True)
+    link_of_author = models.CharField(max_length=500, blank=True, null=True)
+    file = models.FileField(
+        upload_to=get_path_upload_track,
+        validators=[
+            FileExtensionValidator(allowed_extensions=['mp3', 'wev']),
+            validate_size_audio]
+    )
+    create_at = models.DateTimeField(auto_now_add=True)
+    plays_count = models.PositiveIntegerField(default=0)
+    download = models.PositiveIntegerField(default=0)
+    likes_count = models.PositiveIntegerField(default=0)
+    user_of_likes = models.ManyToManyField(
+        UserProfile, related_name='likes_of_tracks')
+
+    def __str__(self):
+        return f"{self.user} - {self.title}"
+
+
+class Comment(models.Model):
+    """Model comment for track"""
+    user = models.ForeignKey(
+        UserProfile, on_delete=models.CASCADE, related_name='comments')
+    track = models.ForeignKey(
+        Track, on_delete=models.CASCADE, related_name='track_comments')
+    text = models.TextField(max_length=1000)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+
+class Playlist(models.Model):
+    """Model playlist user"""
+    user = models.ForeignKey(
+        UserProfile, on_delete=models.CASCADE, related_name='playlists')
+    title = models.CharField(max_length=50)
+    tracks = models.ManyToManyField(Track, related_name='track_playlists')
+    cover = models.ImageField(
+        upload_to=get_path_upload_playlist, blank=True, null=True,
+        validators=[
+            FileExtensionValidator(allowed_extensions=['jpg']),
+            validate_size_image]
+    )
--- clone_sound_cloud/base/services.py
+++ clone_sound_cloud/base/services.py
@@ -3,10 +3,31 @@ from django.core.exceptions import ValidationError
 
 def get_path_upload_avatar(instance, file):
     """Path to file avatar"""
-    return f"avatar/{instance.id}/{file}"
+    return f"avatar/user_{instance.id}/{file}"
+
+
+def get_path_upload_album(instance, file):
+    """Path to file album"""
+    return f"album/user_{instance.id}/{file}"
+
+
+def get_path_upload_track(instance, file):
+    """Path to file album"""
+    return f"track/user_{instance.id}/{file}"
+
+
+def get_path_upload_playlist(instance, file):
+    """Path to file playlist"""
+    return f"playlist/user_{instance.id}/{file}"
 
 
 def validate_size_image(file_obj):
     megabyte_limit = 2
     if file_obj.size > megabyte_limit * 1024 * 1024:
         raise ValidationError(f"Max size for file {megabyte_limit}MB")
+
+
+def validate_size_audio(file_obj):
+    megabyte_limit = 6
+    if file_obj.size > megabyte_limit * 1024 ** 6:
+        raise ValidationError(f"Max size for audio file {megabyte_limit}MB")
