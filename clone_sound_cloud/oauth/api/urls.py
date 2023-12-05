d01a3bd4725ed6bb162eddc9479b792853b8f64e
Maxim Manzik <kepkpkep@gmail.com>
2023-12-05 23:26:04 +0200

    clear code

--- clone_sound_cloud/oauth/api/urls.py
+++ clone_sound_cloud/oauth/api/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('author/', views.AuthorView.as_view({'get': 'list'})),
     path('author/<int:pk>/', views.AuthorView.as_view({'get': 'retrieve'})),
     path('author/<int:pk>/follow-unfollow/', views.FollowAuthorView.as_view()),
-    # path('author/<int:pk>/unfollow/', views.AuthorView.as_view()),
 
     path('social-link/', views.SocialLinkView.as_view(
         {'get': 'list', 'post': 'create'})),
@@ -19,8 +18,4 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('auth/', include('djoser.social.urls')),
-
-    # re_path(r'^auth-social/', include('drf_social_oauth2.urls',
-    # namespace='drf')),
-    # path('auth/', include('djoser.urls.authtoken')),
 ]
